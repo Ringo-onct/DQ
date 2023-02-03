adbb73895b2d03aba4f0bf3b6b1f61e3021e0fd3
Ringo-onct <122839643+Ringo-onct@users.noreply.github.com>
2023-02-03 17:50:12 +0900

    ◆初期状態完成
    ・ある程度ゲームとして成り立つようなところまで完成させた。

--- Code/main.go
+++ Code/main.go
@@ -33,6 +33,7 @@ type monster struct {
 func main() {
 
 	p_sta := new(Status)
+	m_sta := new(Status)
 	p := new(player) //この二つは仮置き。後で名前変える予定
 	m := new(monster)
 
@@ -45,21 +46,21 @@ func main() {
 	p.atk_min = 3
 	m.hp = 50
 	m.atk = 3
-	m.atk = 1
+	m.atk_min = 1
 
 	for true {
 
 		//HP表示
 		fmt.Println("---------------------")
-		fmt.Println("| PLAYER : %4d     |", p.hp)
-		fmt.Println("| MONSTER: %4d     |", m.hp)
+		fmt.Printf("| PLAYER : %4d     |\n", p.hp)
+		fmt.Printf("| MONSTER: %4d     |\n", m.hp)
 		fmt.Println("---------------------")
 
 		fmt.Println("0:にげる")
 		fmt.Println("1:こうげき")
 		fmt.Printf("行動の選択>")
 		fmt.Scan(&p_sta.action)
-
+		fmt.Printf("\n")	//見やすくするための改行
 		//実際の行動
 		//プレイヤーの行動
 		switch p_sta.action {
@@ -69,20 +70,51 @@ func main() {
 		case 1:
 			fmt.Println("プレイヤーのこうげき")
 			//ダメージ計算
-			p.dmg = rand.intn(100)%p.atk + p.atk_min
-			fmt.Println("プレイヤーは%dのダメージをあたえた！", p.dmg)
+			p.dmg = rand.Intn(p.atk) + p.atk_min
+			fmt.Printf("プレイヤーは%dのダメージをあたえた！\n", p.dmg)
 
 		default:
 			fmt.Println("こんらんしている")
 		}
 
-		//プレイヤーの行動
-		switch p_sta.action {
-		//今ここ書いてた
+		//プレイヤーの行動の結果
+		if p_sta.action == 0 {
+			break
+		} else if p_sta.action == 1 {
+			m.hp -= p.dmg
+		}
+
+		//戦闘終了の判定
+		if m.hp <= 0 {
+			fmt.Println("モンスターをたおした！")
+			break
 		}
 
 		//モンスターの行動
-		fmt.Println("ようすをみている")
+		m_sta.action = 1
+		switch m_sta.action {
+		case 1:
+			fmt.Println("モンスターのこうげき")
+			//ダメージの計算
+			m.dmg = rand.Intn(m.atk) + m.atk_min
+			fmt.Printf("モンスターは%dのダメージをあたえた。\n", m.dmg)
+
+		default:
+			fmt.Println("モンスターはようすをみている")
+		}
+
+		//モンスターの行動の結果
+		if m_sta.action == 1 {
+			p.hp -= m.dmg
+		} else {
+			break
+		}
+
+		//戦闘終了の判定
+		if p.hp <= 0 {
+			fmt.Println("プレイヤーはたおれた。。")
+			break
+		}
 
 	}
 }
