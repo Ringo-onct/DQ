5f20ff0cbaf576ef26e16c704f52dbce3cf9c441
Ringo-onct <122839643+Ringo-onct@users.noreply.github.com>
2023-02-03 14:35:20 +0900

    ◆進捗
    ・逃げ出すところまでの処理を追加

--- Code/main.go
+++ Code/main.go
@@ -2,5 +2,33 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Printf("test")
+
+	var(
+		action int
+	)
+
+	for true {
+
+		fmt.Println("0:にげる")
+		fmt.Printf("行動の選択>")
+		fmt.Scan(&action)
+
+		//実際の行動
+		//プレイヤーの行動
+		switch action {
+		case 0:
+			fmt.Println("にげだした。。")
+
+		default:
+			fmt.Println("こんんらんしている")
+		}
+
+		if action == 0 {
+			break
+		}
+
+		//モンスターの行動
+		fmt.Println("ようすをみている\n")
+
+	}
 }
