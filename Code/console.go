7e72af7fcb258f65b0ade2bba4f1903397e6766f
Soragami <reiokikousen@gmail.com>
2023-02-05 11:44:18 +0900

    gotermの実装ができた。うまく動いてる。この後コメントつける。

--- Code/console.go
+++ Code/console.go
@@ -14,6 +14,8 @@ mode	表示する内容をmodeで選択
 func console(p *player, m *monster, mode int) {
 	if mode == 0 { //コンソール画面クリア
 		tm.Clear()
+		tm.MoveCursor(1, 1)
+		tm.Flush()
 	} else if mode == 1 { //体力表示
 		fmt.Println("---------------------")
 		fmt.Printf("| PLAYER : %4d     |\n", p.hp)
