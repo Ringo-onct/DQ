c335f8feca6cb43a786d72d89ea7fb7b70ef3a00
Ringo-onct <122839643+Ringo-onct@users.noreply.github.com>
2023-02-17 04:20:47 +0900

    ◆細部調整
    ・タブがおかしかったのでまったく同じ文を描きなおした。なぜかはわからない。

--- Code/function.go
+++ Code/function.go
@@ -1,45 +1,44 @@
 package main
-
 import (
-    "log"
-    "github.com/mattn/go-tty"
-    "github.com/k0kubun/go-ansi"
+	"log"
 	"fmt"
+
+	"github.com/mattn/go-tty"
+	"github.com/k0kubun/go-ansi"
 )
+func getkey() {
+	key := 0
+	tty, err := tty.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func getkey() int {
-    key := 0
-    tty, err := tty.Open()
-    if err != nil {
-        log.Fatal(err)
-    }
+	defer tty.Close()
 
-    defer tty.Close()
-
-    for {
-        r, _ := tty.ReadRune()
-        if r != 0{
-            if int(r) == 27 {
-                key = 1
-            } else if r == 91 {
-                key = 2
-            } else if key == 2 {
-                switch r {
-                case 65:
-                    return 128  //JISは127までだから、続きに方向キーを割り振った形。
-                case 66:
-                    return 129
-                case 67:
-                    return 130
-                case 68:
-                    return 131
-                }
-                key = 0
-            } else {
-                return int(r)
-            }
-        }
-    }
+	for {
+		r, _ := tty.ReadRune()
+		if r != 0 [
+			if int(r) == 27 {
+				key = 1
+			} else if r == 91 {
+				key = 2
+			} else if key == 2 {
+				switch r {
+				case 65:
+					return 128
+				case 66:
+					return 129
+				case 67:
+					return 130
+				case 68:
+					return 131
+				}
+				key = 0
+			} else {
+				return int(r)
+			}
+		]
+	}
 }
 
 func chose(line int) int{
