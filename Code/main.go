a54014d809a59b4aacf6d99087e7ea84f48bb479
Ringo-onct <122839643+Ringo-onct@users.noreply.github.com>
2023-02-19 03:54:15 +0900

    ◆仕様変更
    ・構造体要素を変更した。
    ・構造体要素変更に伴い、ファイル内の構造体変数を変更。
    ・構造体要素変更に伴い、monster_listとplayer_listの内容を変更した。
    
    詳しいことは、issue11か、このコミットに対するコメント、プルリクエストに書いておくので確認してください。

--- Code/main.go
+++ Code/main.go
@@ -6,24 +6,27 @@ import (
 
 type status struct {	//小文字にしたら、Goのパッケージ内の関数に小文字から始まる関数内から、被らないらしい。
 	name	string	//名前
-	hp		int		//体力
-	mp		int		//MP
 	atk		int		//攻撃力
-	atk_min int		//最低攻撃力
-	def		int		//防御力
-	luk		int		//運
-	action	int		//行動
-	dmg		int		//与えたダメージ
-	exp		int 	//味方:蓄積exp 敵：撃破時exp
-	gld		int 	//金
+	dmg		int		//与えるダメージ
+	dif		int		//守備力・素早さ
+	hp		int		//体力
+	hp_max	int		//最大体力
+	mp		int		//pはそのままmp,mはマホの回避率
+	exp		int		//pは総Exp,mは獲得exp
+	gold	int		//pは総gold,mは獲得gold
+	lari	int		//pはレベル,mはラリの回避率
+	gira	int		//pはなし,mはギラの回避率
+	avo		int		//pはなし。mは回避率
 }
 
 func main() {
 	line := linecountP()
 	p_sta := make([]status, line)
 	//一応、今作ってるのはplayerのデータ読み込みだから、monsterはまだ配列対応させない。
-	var m_sta status
-
+	var (
+		m_sta status
+		action int
+	)
 	//ゲーム開始待機時のコンソール画面表示
 	console(&p_sta[0], &m_sta, 5)
 	console(&p_sta[0], &m_sta, 0)
@@ -38,71 +41,68 @@ func main() {
 
 	//player選択
 	pl := prompt(&p_sta[0], 2) - 1
-	console(&p_sta[0], &m_sta, 0)
+	console(&p_sta[pl], &m_sta, 0)
 
 
 	fileP(&p_sta[pl], pl + 1)	//playerデータ読み込みここに置くと再読み込みさせないで体力保持できる
 	//↑これも後で消す
-	time.Sleep(2 * time.Second)
-	for true {	//戦闘継続ループ
-	console(&p_sta[pl], &m_sta, 0)
 	time.Sleep(1 * time.Second)
-	fileM(&m_sta)	//monsterデータ読み込み
-	console(&p_sta[pl], &m_sta, 3)
-	time.Sleep(2 * time.Second)
 
-	for true {
-
-		console(&p_sta[pl], &m_sta, 0)	//コンソール画面クリア
-		console(&p_sta[pl], &m_sta, 1)	//体力表示
-		prompt(&p_sta[pl], 1)	//行動選択
-
+	for true {	//戦闘継続ループ
+		console(&p_sta[pl], &m_sta, 0)
 		time.Sleep(1 * time.Second)
+		fileM(&m_sta)	//monsterデータ読み込み
+		console(&p_sta[pl], &m_sta, 3)
+		time.Sleep(2 * time.Second)
 
-		console(&p_sta[pl], &m_sta, 0)	//コンソール画面クリア
+		for true {	//戦闘処理
+			console(&p_sta[pl], &m_sta, 0)	//コンソール画面クリア
+			console(&p_sta[pl], &m_sta, 1)	//体力表示
 
-		//playerの行動
-		actionP(&p_sta[pl])
+			//playerの行動選択
 
-		//プレイヤーの行動の結果
-		if p_sta[pl].action == 0 {
+			//プレイヤーの行動選択・結果
+			action = prompt(&p_sta[pl], 1)
+
 			time.Sleep(1 * time.Second)
 			console(&p_sta[pl], &m_sta, 0)
-			break
-		} else if p_sta[pl].action == 1 {
-			m_sta.hp -= p_sta[pl].dmg
-		}
 
-		//勝敗判定
-		if console(&p_sta[pl], &m_sta, 2) == 1 {
-			break
-		}
+			if action == 0 {		//戦闘離脱
+				actionP(&p_sta[pl], 0)
+				time.Sleep(1 * time.Second)
+				console(&p_sta[pl], &m_sta, 0)
+				break
+			} else if action == 1 {	//戦闘後のダメージ処理
+				actionP(&p_sta[pl], 1)
+				m_sta.hp -= p_sta[pl].dmg
+			}
 
-		//モンスターの行動
-		m_sta.action = 1
-		actionM(&m_sta)
+			//勝敗判定
+			if console(&p_sta[pl], &m_sta, 2) == 1 {	//勝ち
+				break
+			}
 
-		//モンスターの行動の結果
-		if m_sta.action == 1 {
+			time.Sleep(1 * time.Second)
+			//モンスターの行動
+			actionM(&m_sta)
+
+			//モンスターの行動の結果
 			p_sta[pl].hp -= m_sta.dmg
-		} else {
-			break
+
+			//勝敗判定
+			if console(&p_sta[pl], &m_sta, 2) == 2 {
+				time.Sleep(1 * time.Second)
+				break
+			}
+
+			time.Sleep(3 * time.Second)
 		}
 
-		//勝敗判定
-		if console(&p_sta[pl], &m_sta, 2) == 2 {
+		if p_sta[pl].hp <= 0 {
+			break
+		} else if prompt(&p_sta[pl], 0) == 0 {
 			break
 		}
-
-		time.Sleep(3 * time.Second)
-
-	}
-
-	if prompt(&p_sta[pl], 0) == 0 {
-		break
-	} else {
-		p_sta[pl].hp += 10
-	}
 	}
 	console(&p_sta[pl], &m_sta, 0)
 	console(&p_sta[pl], &m_sta, 4)
